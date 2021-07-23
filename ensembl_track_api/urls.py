83f66db0bc0bd64aaad38c92c6af489988350873
Andres Veidenberg <andres@ebi.ac.uk>
2021-07-23 09:59:31 +0300

    Cleanup

--- ensembl_track_api/urls.py
+++ ensembl_track_api/urls.py
@@ -4,9 +4,7 @@ URL Configuration for Ensembl Track API endpoint
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 """
-from django.contrib import admin
 from django.urls import path, include
-from .settings import ADMIN_URL
 
 urlpatterns = [
     path("track_categories/", include("tracks.urls", namespace="tracks")),
