4f79fb1242ccf02512c88d7650cceb7d2c991c22
Andres Veidenberg <andres@ebi.ac.uk>
2021-09-28 14:38:54 +0300

    Improve support for data updates

--- utils/grant_access.sh
+++ utils/grant_access.sh
@@ -1,4 +1,4 @@
 # Commands to give the read-only k8s user access to the tables created under the read/write user db schema.
 # Run this after the database inititation and every time tables are altered (Django migration commands).
-psql -h <DB_HOST> -p 5432 -d <DB_NAME> -U <RW_USER> -W # Fill in the details
+psql -h <DB_HOST> -p 5432 -d <DB_NAME> -U <RW_USER> -W # Fill in the details (or run './manage.py dbshell' to use settings.py)
 GRANT SELECT ON ALL TABLES IN SCHEMA <RW_USER> TO <RO_USER>; # Fill the gaps, run in the psql shell
