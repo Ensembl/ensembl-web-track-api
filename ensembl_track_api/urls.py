2e3257dfd5a82d60de94be099e23ac8f1500dd39
Andres Veidenberg <andres@ebi.ac.uk>
2023-09-09 23:04:53 +0300

    Fix URL root

--- ensembl_track_api/urls.py
+++ ensembl_track_api/urls.py
@@ -4,5 +4,5 @@ URL Configuration for Ensembl Track API endpoint
 from django.urls import path, include
 
 urlpatterns = [
-    path("/", include("tracks.urls", namespace="tracks")),
+    path("", include("tracks.urls", namespace="tracks")),
 ]
