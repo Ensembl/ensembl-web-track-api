a2c7da2969b6524357ba588518a71821aa835588
Andres Veidenberg <andres@ebi.ac.uk>
2023-10-23 14:20:03 +0300

    Update serializers

--- tracks/serializers.py
+++ tracks/serializers.py
@@ -18,15 +18,25 @@ class BaseTrackSerializer(serializers.ModelSerializer):
         fields = ["track_id", "label", "colour", "trigger", "type",
             "display_order", "on_by_default", "sources"]
 
+# track payload in "track_categories" endpoint (consumed by client)
 class CategoryTrackSerializer(BaseTrackSerializer):
     class Meta(BaseTrackSerializer.Meta):
         fields = BaseTrackSerializer.Meta.fields + ["additional_info", "description"]
 
+# track payload in "track" endpoint (consumed by genome browser)
 class ReadTrackSerializer(BaseTrackSerializer):
     class Meta(BaseTrackSerializer.Meta):
         fields = BaseTrackSerializer.Meta.fields + ["datafiles"]
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["label", "track_category_id", "category_type"]
+
+# track submission payload
 class WriteTrackSerializer(BaseTrackSerializer):
+    category = CategorySerializer(write_only=True)
+
     class Meta(BaseTrackSerializer.Meta):
         fields = BaseTrackSerializer.Meta.fields + ["genome_id", "datafiles", "additional_info", "description"]
     
@@ -36,12 +46,12 @@ class WriteTrackSerializer(BaseTrackSerializer):
         track = Track.objects.create(category=category, **validated_data)
         return track
 
-class CategorySerializer(serializers.ModelSerializer):
+# payload wrappers in "track_categories" endpoint
+class TracksCategorySerializer(CategorySerializer):
     track_list = CategoryTrackSerializer(many=True, read_only=True)
 
     class Meta:
-        model = Category
-        fields = ["label", "track_category_id", "category_type", "track_list"]
+        fields = CategorySerializer.Meta.fields + ["track_list"]
 
 class CategoryListSerializer(serializers.ModelSerializer):
-    track_categories = CategorySerializer(many=True, read_only=True)
+    track_categories = TracksCategorySerializer(many=True, read_only=True)
