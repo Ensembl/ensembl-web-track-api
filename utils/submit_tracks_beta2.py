3ecc296daabee1e6cb1ac6dfb18bd23e339586ae
Andres Veidenberg <andres@ebi.ac.uk>
2024-07-24 20:35:51 +0300

    Small fixes

--- utils/submit_tracks_beta2.py
+++ utils/submit_tracks_beta2.py
@@ -1,30 +1,45 @@
+#!/usr/bin/env python3
+
 import argparse
 import glob
 import os.path
 import requests
+from uuid import UUID
 import yaml
 
-args:argparse.Namespace = argparse.Namespace()
+args = argparse.Namespace()
+template_dir = os.path.join(os.path.dirname(__file__), "..", "templates")
+
 def process_input_parameters():
   parser = argparse.ArgumentParser(description='Submit tracks to Track API based on input datafiles')
-  parser.add_argument('-e', '--env', choices=['local', 'dev', 'staging', 'prod'], default='dev', metavar='ENV', help='target Track API deployment')
+  parser.add_argument('-e', '--env', choices=['local', 'dev', 'staging', 'prod'],
+                      default='dev', help='target Track API deployment (default: %(default)s)')
   parser.add_argument('-g', '--genome', nargs='*', metavar='GENOME_UUID', help='limit to specific genomes')
   parser.add_argument('-f', '--file', nargs='*', metavar='FILENAME', help='limit to specific tracks (datafiles)')
   parser.add_argument('-t', '--template', nargs='*', metavar='TEMPLATE', help='limit to specific track template')
   parser.add_argument('-d', '--dry-run', action='store_true', help='do not submit tracks, just print them')
-  parser.add_argument('data_dir', nargs='?', default='/hps/nobackup/flicek/ensembl/infrastructure/arne/e2020-datafile-2024-03', 
-    help='root directory containing the datafiles (in genome subdirectories)')
-  # repeat tracks: /hps/nobackup/flicek/ensembl/infrastructure/repeats-beta/results
-  # other genomic tracks: /hps/nobackup/flicek/ensembl/infrastructure/arne/e2020-datafile-2024-03
-  args = parser.parse_args(namespace=args)
+  parser.add_argument('data_dir', metavar='DATA_DIR', help='root directory containing the datafiles (in genome subdirectories)')
+  #repeat tracks: /hps/nobackup/flicek/ensembl/infrastructure/repeats-beta/results
+  #other genomic tracks: /hps/nobackup/flicek/ensembl/infrastructure/arne/e2020-datafile-2024-03
+  parser.parse_args(namespace=args)
 
 # 1) Loop through all the available bigbed/bigwig datafiles
 def process_bigbeds(root_dir: str) -> None:
-  for subdir, dirs, files in os.walk(root_dir):
+  if not os.path.isdir(root_dir):
+    print(f"Error: data directory {root_dir} not found")
+    exit(1)
+  for subdir in os.listdir(root_dir):
+    if not os.path.isdir(f"{root_dir}/{subdir}"):
+      continue
+    try:
+      UUID(subdir, version=4)
+    except ValueError:
+      print(f"Skipping non-genome directory {subdir}")
+      continue
     if args.genome and subdir not in args.genome:
       continue
     print(f"Processing genome {subdir}")
-    for file in files:
+    for file in os.listdir(f"{root_dir}/{subdir}"):
       if file.endswith('.bb') or file.endswith('.bw'):
         find_template(subdir, file)
 
@@ -33,7 +48,7 @@ def find_template(genome_id: str, datafile: str) -> None:
   if args.file and datafile not in args.file:
     return
   filename = os.path.splitext(datafile)[0]
-  template_files = [os.path.basename(f) for f in glob.glob("../templates/*.yaml")]
+  template_files = [os.path.basename(f) for f in glob.glob(f"{template_dir}/*.yaml")]
   if filename in template_files: # 1-to-1 match
     return apply_template(genome_id, datafile, filename)
   for template_file in template_files: # multiple tracks (templates) per datafile
@@ -44,15 +59,15 @@ def find_template(genome_id: str, datafile: str) -> None:
 def apply_template(genome_id: str, filename: str, template_file: str) -> None:
   if args.template and template_file not in args.template:
     return
-  print(f"Applying {template_file} to {filename}")
-  with open(template_file, 'r') as file:
+  print(f"{filename} => {template_file}")
+  with open(f"{template_dir}/{template_file}", 'r') as file:
     track_data: dict = yaml.safe_load(file)
   track_data['genome_id'] = genome_id
   submit_track(track_data)
 
 # 4) Submit the track payload to Track API
 def submit_track(track_data: dict, second_try: bool = False) -> None:
-  print(f"Submitting track {track_data['label']}")
+  print(f"Submitting track: {track_data['label']}")
   if args.dry_run:
     print(track_data)
     return
@@ -78,6 +93,6 @@ def submit_track(track_data: dict, second_try: bool = False) -> None:
     exit(1)
   print(msg)  # expected response: {"track_id": "some-uuid"}
 
-def main():
+if __name__ == '__main__':
   process_input_parameters()
   process_bigbeds(args.data_dir)
