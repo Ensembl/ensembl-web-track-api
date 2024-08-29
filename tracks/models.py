72a481bb0766b2060780425b2e2db7e07ce2697b
Andres Veidenberg <andres@ebi.ac.uk>
2024-08-29 12:39:11 +0300

    Increase URL name length in db

--- tracks/models.py
+++ tracks/models.py
@@ -34,7 +34,7 @@ class Track(models.Model):
 
 class Source(models.Model):
     track = models.ManyToManyField(Track, related_name="sources")
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     url = models.URLField()
 
     class Meta:
