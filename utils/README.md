57c7aee766daf8be984119e96a81e275fddff19d
Andres Veidenberg <andres@ebi.ac.uk>
2025-03-12 20:40:34 +0000

    Integrate gene desc script, cleanup, refactoring

--- utils/README.md
+++ utils/README.md
@@ -1,29 +1,24 @@
-## Utility scripts
-
-### Bulk data updater
+## Bulk data updater for Track API
 
 `submit_tracks.py` script constructs and submits track payloads to Track API `POST` endpoint to add new tracks.
-It uses the yaml templates in `/templates` to fill the payload fields. You specify the submitted track types (templates) with  `--template` parameter, 
-or by pointing it to a data directory (by setting `TRACK_API_DIR` environment variable). In the latter case the script tries to sync the data directory
-to Track API by matching each datafile (with `.bb`, `.bw` extension) with corresponding template file: a template filename must match or start with the datafile name to be submitted.
-The datafile parent directory names are used to fill in the genome UUID field (override via `--genome` param).
+It uses the yaml templates in `/templates` and some supplemental data (from metadata db, csv files and track datafile directory) to construct the payloads.
+The only required params are `--release` (release id) and `--env` (environment name, defaults to 'dev').
+`--release` sets the list of genomes to be loaded, and `--env` sets the track datafiles dir (for list of track types to be loaded) and Track API endpoint URL.
+The genomes list can be trimmed down with `--genomes` param, and the list of loaded track types with `--templates`.
+The datadir path and Track API URL set can be overriden with envvars (see below).
+Unless `--templates` is set, the script infers the list of tracks for each genome from the datafile names in the data dir. More specifically, it matches each datafile (with `.bb`, `.bw` extension) with corresponding template file: a template filename must match or start with the datafile name to be submitted.
 
 Example:
 ```bash
-export TRACK_API_URL=http://localhost:8000 # target track API
-export TRACK_DATA_DIR=/Users/Alice/datafiles # track datafiles
-./utils/submit_tracks.py #submit tracks for all datafiles in data dir
+export TRACK_DATA_DIR=/Users/Alice/datafiles # override track datafiles location
+export TRACK_API_URL=http://localhost:8000 # override target track API
+./utils/submit_tracks.py --release 5 #submit tracks the genomes in the release
 ```
 For more detailed instructions for production, refer to [ENSWEBSOPS-171](https://www.ebi.ac.uk/panda/jira/browse/ENSWEBSOPS-171).
 
+#### Note:
+For some some tracks, species-specific values are used to fill out templates (e.g. track descriptions).
 
-#### Data sources
-Data submission script combines input from multiple sources:
-| Source | Where | Why |
-|-------|-------|------|
-| Templates | `/tempaltes/*.yaml` | Track payload base |
-| Overrides | `/tempaltes/*.csv` | Species-specific values |
-| Datafiles | Input data dir | Specify species/tracks to submit |
-
-- The datafiles source can be replaced/modified with command-line params (`-g`,`-t`)
-- Gene track overrides (`gene-track-desc.csv`) was generated/updated from Metadata API with `utils/get_analysis_descs.py` script. `variation-track-desc.csv` was converted from input JSON (handed over from Variation team).
+Data sources:
+* Gene tracks: metadata API (using `get_gene_desc.py`)
+* Variation tracks: `/templates/variant-track-desc.csv` (manually updated by Variation)
--- utils/submit_tracks.py
+++ utils/submit_tracks.py
@@ -2,9 +2,9 @@
 
 """
 Script for submitting track records to Track API. In a nutshell, the script:
-1) Fills in track templates (loaded from yaml files) from CLI params, CSV files and filenames in TRACK_DATA_DIR
-2) Submits the result as JSON payloads to TRACK_API_URL
-See the params below for more granulated track loading (specific release, genomes, tracks etc.)
+1) Fills in track templates based on CLI params, metadata (from db & csv), and filenames in a data dir
+2) Submits the resulting track records as JSON payloads to Track API REST endpoint
+Only required parameter is release nr. See below for additional options.
 """
 
 import argparse
@@ -16,9 +16,9 @@ from typing_extensions import NotRequired, TypedDict
 from uuid import UUID
 import yaml
 
-from . import create_gene_csv
+from get_gene_track_desc import main  as get_gene_desc
 
-# Datamodels for static typing (runtime checks done by Track API)
+# Datamodel for track payloads
 class TrackData(TypedDict):
     category: str
     genome_id: str
@@ -32,31 +32,49 @@ class TrackData(TypedDict):
     trigger: list[str]
     type: str
 
+# Datamodel for track descriptions
+class DescData(TypedDict):
+    description: str
+    source_names: list[str]
+    source_urls: list[str]
+    species: NotRequired[str]
+    track_name: NotRequired[str]
 
-class CSVRow(TypedDict):
-    desc: str
-    name: str
-    sources: list[str]
-    urls: list[str]
+Descriptions = dict[str, DescData]
+DescCollection = dict[str, Descriptions]
 
-
-CSVData = dict[str, CSVRow]
-CSVCollection = dict[str, CSVData]
+# Global variables / constants
+suf = "/nfs/public/ro/enswbsites_codon/newsite"
+pref = "genome_browser/8"
+ENV = {
+    "dev": {
+        "data_dir": f"{suf}/dev/{pref}",
+        "track_api_url": "https://dev-2020.ensembl.org"
+    },
+    "staging": {
+        "data_dir": f"{suf}/staging/{pref}",
+        "track_api_url": "https://staging-2020.ensembl.org"
+    },
+    "prod": {
+        "data_dir": f"{suf}/live/{pref}",
+        "track_api_url": "https://beta.ensembl.org"
+    },
+}
 
 args = argparse.Namespace()
 template_dir = os.path.join(os.path.dirname(__file__), "..", "templates")
-GENE_CSV_FILE = f"{template_dir}/gene-track-desc.csv"
 VARIANT_CSV_FILE = f"{template_dir}/variant-track-desc.csv"
 EXT = ".yaml"
 templates = [
     os.path.basename(f).replace(EXT, "") for f in glob.glob(f"{template_dir}/*{EXT}")
 ]
-track_api_url = os.environ.get("TRACK_API_URL", "")
-data_dir = os.environ.get("TRACK_DATA_DIR", "")
-csv_data: CSVCollection = {}
+data_dir = ""
+track_api_url = ""
+metadata: DescCollection = {"gene":{}, "variant":{}}
 logfile = None
 
 
+# Helper functions
 def fail(msg):
     if msg:
         print(msg)
@@ -68,76 +86,80 @@ def fail(msg):
 
 def process_input_parameters():
     global args, track_api_url, data_dir
+    prog = os.path.basename(__file__)
+    envs = list(ENV.keys())
     parser = argparse.ArgumentParser(
-        description="Submit tracks to Track API based on input datafiles",
+        description="Submit Genome Browser tracks to Track API",
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog="""
-Required environment variables: 
-  - TRACK_API_URL: Track API endpoint (e.g. https://dev-2020.ensembl.org/api/tracks)
-    Can be omitted for dry-run mode (-d)
-  - TRACK_DATA_DIR: Directory containing track datafiles (in genome ID subdirectories)
-    Can be omitted if a track list is specified with -g and -f or -t
+        epilog=f"""
+Submits track records (based on yaml templates) to Track API endpoint.
+For input, it needs a list of genome UUIDs (derived from --release param), 
+list of track types, and the target endpoint URL (both derived from --env param).
+Optional environment variables (overrides defaults from --env): 
+  - TRACK_API_URL: Track API endpoint URL. Not used in dry-run mode (-d).
+  - TRACK_DATA_DIR: Directory containing track datafiles.
+    Not used when the track types are specified with -f or -t.
 
 Examples:
-  - Submit tracks for all datafiles in TRACK_DATA_DIR (skipping existing tracks): submit_tracks.py
-  - Same as above, but limit to genomes in release 5: submit_tracks.py --release 5
-  - Resubmit all tracks for dog and pig: submit_tracks.py -o -g 2284d28a-2cf7-41f0-bed6-0982601f7888 a7335667-93e7-11ec-a39d-005056b38ce3
-  - Submit gene tracks for dog (TRACK_DATA_DIR not needed): submit_tracks.py -t transcripts -g 2284d28a-2cf7-41f0-bed6-0982601f7888
+  - Submit all tracks for release beta-5 to dev: {prog} --release 5
+  - Resubmit all tracks for dog and pig: {prog} -r 5 -o -g 2284d28a-2cf7-41f0-bed6-0982601f7888 a7335667-93e7-11ec-a39d-005056b38ce3
+  - Submit gene tracks for dog (data dir not used): {prog} -r 5 -t transcripts -g 2284d28a-2cf7-41f0-bed6-0982601f7888
   """,
+    )
+    parser.add_argument(
+        "-r",
+        "--release",
+        metavar="RELEASE",
+        type=int,
+        required=True,
+        help="load tracks for a specific release number",
+    )
+    parser.add_argument(
+        "-e",
+        "--env",
+        choices=envs,
+        default=envs[0], #dev
+        const=envs[0],
+        nargs="?",
+        type=str,
+        help="source/target environment (default: %(default)s)",
     )
     parser.add_argument(
         "-f",
         "--file",
         nargs="*",
-        metavar="FILENAME",
+        action="extend",
+        metavar="FILENAMES",
         help="limit to specific track datafiles",
     )
     parser.add_argument(
-        "-t",
-        "--template",
+        "-g",
+        "--genomes",
         nargs="*",
-        metavar="TEMPLATE",
-        help="limit to specific track templates (types)",
+        action="extend",
+        metavar="GENOME_IDS",
+        help="limit to specific genome UUIDs",
     )
     parser.add_argument(
-        "-e",
-        "--exclude",
+        "-t",
+        "--templates",
         nargs="*",
-        metavar="TEMPLATE",
-        help="exclude specific track templates (types)",
+        action="extend",
+        help="limit to specific track types (templates)",
     )
     parser.add_argument(
         "-c",
         "--continue",
         metavar="GENOME_ID",
-        help="continue (resume) track loading from a specific genome ID)",
+        help="resume track loading from a specific genome ID",
     )
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument(
-        "-g",
-        "--genome",
-        nargs="*",
-        metavar="GENOME_ID",
-        help="limit to specific genomes",
-    )
-    group.add_argument(
-        "-r",
-        "--release",
-        metavar="RELEASE",
-        type=int,
-        help="load tracks for a specific release number",
-    )
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument(
+    parser.add_argument(
         "-d",
         "--dry-run",
         action="store_true",
-        help="do not submit tracks, just print the payload",
+        help="do not submit tracks, just print the payloads",
     )
-    group.add_argument(
-        "-o", "--overwrite", action="store_true", help="overwrite (all) existing tracks"
-    )
-    group.add_argument(
+    parser.add_argument(
         "-l",
         "--logfile",
         metavar="FILENAME",
@@ -145,16 +167,21 @@ Examples:
         help="log progress to a file (use '' for no logfile, default: %(default)s)",
     )
     parser.add_argument(
+        "-o", "--overwrite", action="store_true", help="overwrite (all) existing tracks"
+    )
+    parser.add_argument(
         "-q", "--quiet", action="store_true", help="suppress status messages"
     )
 
     parser.parse_args(namespace=args)
-    if not track_api_url and not args.dry_run:
-        fail("Error: TRACK_API_URL environment variable not set")
-    if not data_dir and not (args.genome and (args.file or args.template)):
-        fail("Error: TRACK_DATA_DIR environment variable not set")
-    if args.template:
-        args.template = [t.replace(EXT, "") for t in args.template]
+    if not args.release:
+        fail("Error: Please provide a release number.")
+    if args.env not in ENV:
+        fail(f"Error: Invalid environment name (expected: {' | '.join(envs)}).")
+    track_api_url = os.getenv("TRACK_API_URL", ENV[args.env]["track_api_url"])
+    data_dir = os.getenv("TRACK_DATA_DIR", ENV[args.env]["data_dir"])
+    if args.templates:
+        args.templates = [t.replace(EXT, "") for t in args.templates]
 
 
 # Print messages in verbose mode
@@ -164,32 +191,18 @@ def log(msg: object) -> None:
     if logfile:
         print(msg, file=logfile)
 
-
-# Limit tracks to those specified in command-line args
-def filter_templates() -> None:
-    global templates
-    if args.template:
-        templates = [
-            t for t in templates if any(t.startswith(i) for i in args.template)
-        ]
-    if args.exclude:
-        templates = [
-            t for t in templates if not any(t.startswith(e) for e in args.exclude)
-        ]
-
-
-# Read species-specific track template fields from CSV file
-def parse_csv(path: str) -> CSVData:
-    data: CSVData = {}
+# Read species-specific track descriptions from CSV file
+def parse_csv(path: str) -> Descriptions:
+    data: Descriptions = {}
     try:
         with open(path) as f:
             reader = csv.DictReader(f)
             for line in reader:
                 data[line["Genome_UUID"]] = {
-                    "desc": line["Description"],
-                    "name": line["Track_name"] if "Track_name" in line else "",
-                    "sources": line["Source_name"].split(","),
-                    "urls": line["Source_URL"].split(","),
+                    "description": line["Description"],
+                    "track_name": line["Track_name"] if "Track_name" in line else "",
+                    "source_names": line["Source_name"].split(","),
+                    "source_urls": line["Source_URL"].split(","),
                 }
     except FileNotFoundError:
         fail(f"Error: track description CSV file not found in {path}")
@@ -199,13 +212,17 @@ def parse_csv(path: str) -> CSVData:
 
 
 # Track loading process:
-# 1) Loop through all track datafiles in the data directory
+# 1) Option A: loop through the track datafiles in the data directory
 def process_data_dir() -> None:
     if not os.path.isdir(data_dir):
         fail(f"Error: data directory {data_dir} not found")
     subdirs = os.listdir(data_dir)
     i = 0
-    total = len(args.genome or subdirs)
+    total = len(args.genomes or subdirs)
+    if args.genomes:
+        missing_genomes = [genome for genome in args.genomes if genome not in subdirs]
+        if missing_genomes:
+            fail(f"Error: Genome(s) missing in data directory: {', '.join(missing_genomes)}")
     for subdir in subdirs:
         if not os.path.isdir(f"{data_dir}/{subdir}"):
             continue
@@ -214,7 +231,7 @@ def process_data_dir() -> None:
         except ValueError:
             log(f"Skipping non-genome directory {subdir}")
             continue
-        if args.genome and subdir not in args.genome:
+        if args.genomes and subdir not in args.genomes:
             continue
         i += 1
         if args.resume:
@@ -231,11 +248,16 @@ def process_data_dir() -> None:
                 match_template(subdir, file)
 
 
-# 1b) ...or use a list of tracks specified in command-line args
+# 1) Option B: use the list of tracks from command-line args
 def process_track_list() -> None:
-    files = args.template or args.file
-    total = len(args.genome)
-    for i, genome_id in enumerate(args.genome):
+    global templates
+    if args.templates:
+        templates = [
+            t for t in templates if any(t.startswith(i) for i in args.templates)
+        ]
+    files = args.templates or args.files
+    total = len(args.genomes)
+    for i, genome_id in enumerate(args.genomes):
         if args.resume:
             if genome_id != args.resume:
                 log(f"Skipping genome {genome_id} ({i+1}/{total})")
@@ -249,9 +271,9 @@ def process_track_list() -> None:
             match_template(genome_id, filename)
 
 
-# 2) Load the track payload template(s) for each datafile
+# 2) Load the payload template(s) for each track type (datafile name)
 def match_template(genome_id: str, datafile: str) -> None:
-    if args.file and not args.template and datafile not in args.file:
+    if args.files and not args.templates and datafile not in args.files:
         return
     filename = os.path.splitext(datafile)[0]
     # skip variant focus tracks and redundant bigwig files
@@ -277,7 +299,7 @@ def match_template(genome_id: str, datafile: str) -> None:
         log(f"Warning: No track template found for {datafile}")
 
 
-# 3) Fill in the template (update variable fields)
+# 3) Fill in the template (update variable fields/placeholders)
 def apply_template(genome_id: str, template_name: str, datafile: str = "") -> None:
     with open(f"{template_dir}/{template_name}{EXT}", "r") as template_file:
         track_data: TrackData = yaml.safe_load(template_file)
@@ -291,29 +313,29 @@ def apply_template(genome_id: str, template_name: str, datafile: str = "") -> No
                 track_data["datafiles"][key] = f"{nameroot}-summary.bw"
             else:
                 track_data["datafiles"][key] = datafile
-    # udpate species-specific fields (gene & variation tracks)
+    # update species-specific template fields (for gene & variation tracks)
     if template_name.startswith("transcripts") or template_name.startswith("variant"):
         track_type = "gene" if template_name.startswith("transcripts") else "variant"
-        if genome_id in csv_data[track_type]:
-            row = csv_data[track_type][genome_id]
-            if row["name"]:
-                track_data["label"] = row["name"]
-            if row["desc"]:
+        if genome_id in metadata[track_type]:
+            row = metadata[track_type][genome_id]
+            if row["track_name"]:
+                track_data["label"] = row["track_name"]
+            if row["description"]:
                 if track_type == "gene":
-                    if row["sources"][0]:
+                    if row["source_names"][0]:
                         track_data[
                             "description"
-                        ] += f"\nGenes {'annotated by' if row['desc']=='Annotated' else 'imported from'}  {row['sources'][0]}."
+                        ] += f"\nGenes {'annotated by' if row['description']=='Annotated' else 'imported from'}  {row['source_names'][0]}."
                 else:
-                    track_data["description"] = row["desc"]
-            if row["sources"][0] and len(row["sources"]) == len(row["urls"]):
+                    track_data["description"] = row["description"]
+            if row["source_names"][0] and len(row["source_names"]) == len(row["source_urls"]):
                 if "sources" not in track_data:
                     track_data["sources"] = []
-                for i, source_name in enumerate(row["sources"]):
-                    if not source_name or not row["urls"][i]:
+                for i, source_name in enumerate(row["source_names"]):
+                    if not source_name or not row["source_urls"][i]:
                         continue
                     track_data["sources"].append(
-                        {"name": source_name, "url": row["urls"][i]}
+                        {"name": source_name, "url": row["source_urls"][i]}
                     )
         elif track_type == "gene":
             log("Warning: Missing gene track descriptions.")
@@ -358,20 +380,19 @@ def delete_tracks(genome_id: str) -> None:
 if __name__ == "__main__":
     # setup
     process_input_parameters()
-    filter_templates()
 
-    # dump gene track metadata to CSV
-    if args.release:
-        create_gene_csv.OUTFILENAME = GENE_CSV_FILE
-        create_gene_csv.RELEASE_ID = args.release
-        create_gene_csv.main()
-
-    # read species-specific track info from CSV
-    csv_data["gene"] = parse_csv(GENE_CSV_FILE)
-    csv_data["variant"] = parse_csv(VARIANT_CSV_FILE)
-    # limit to genomes in specified release
-    if args.release:
-        args.genome = csv_data["gene"].keys()
+    # get gene/variant tracks metadata (track descriptions)
+    metadata["gene"] = get_gene_desc(release=args.release, genomes=args.genomes)
+    metadata["variant"] = parse_csv(VARIANT_CSV_FILE)
+    if not metadata["gene"]:
+        fail(f"Error: No genomes found in release {args.release}.")
+    # limit to genomes in the release
+    if not args.genomes:
+        args.genomes = list(metadata["gene"].keys())
+    else:
+        unexpected_genomes = [g for g in args.genomes if g not in metadata["gene"]]
+        if unexpected_genomes:
+            fail(f"Error: These genomes are not part of release {args.release}: {', '.join(unexpected_genomes)}")
 
     if args.logfile:
         try:
@@ -384,7 +405,7 @@ if __name__ == "__main__":
         log(f"Submitting tracks to {track_api_url}")
     if data_dir:
         process_data_dir()
-    elif args.genome and (args.file or args.template):
+    elif args.genomes and (args.files or args.templates):
         process_track_list()
     else:
         fail(
